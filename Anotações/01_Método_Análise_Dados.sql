bbc1d999005d53e0361dc726e9c40ee3319d63e3
Matheus Lopes <mat.lopes2907@gmail.com>
2025-04-08 20:21:26 -0300

    Categorização dos Arquivos

---
+++ Anotações/01_Método_Análise_Dados.sql
@@ -0,0 +1,70 @@
+/*
+    1ª DEFINIR OBJETIVO
+    2ª ESCOLHER OS DADOS NO PRINCÍPIO 80/20
+    3ª COMO OS 80% AFETAM OS INDICADORES
+    4ª VISUALIZAÇÃO DE DADOS PARA ANALISE
+    5ª QUESTIONAR E ENTENDER OS DADOS
+    6ª CONFIRMAR OS DADOS
+*/
+
+CASE DE SITUAÇÃO: AJUDAR UMA FÁBRICA A AUMENTAR A PRODUTIVIDADE 
+
+-- 1ª DEFINIR OS OBJETIVOS
+    DEFINIR AS PRINCIPAIS PERGUNTAS: "POR QUE O LUCRO AUMENTOU / CAIU?"
+    QUAIS PROBLEMAS QUERO RESOLVER: "A RECEITA CAIU?" "OS CUSTOS AUMENTARAM?"
+    OBJETIVO: AUMENTAR A LUCRATIVIDADE DA EMPRESA
+    ENTENDER O NEGÓCIO DA INSTITUIÇÃO!
+
+-- 2ª ESCOLHER OS DADOS NO PRINCÍPIO 80/20
+QUAIS SÃO OS FATORES MAIS RELEVANTES QUE INFLUENCIAM OS DADOS E INDICADORES
+
+PRINCÍPIO DE PARETO
+"80%  DAS TERRAS SÃO DE PROPRIEDADE DE 20% DA POPULAÇÃO"
+
+ISSO TRAZ A IDEIA DE QUE OS RESULTADOS SÃO MOTIVADOS QUASE SEMPRE PELAS 20% DAS CAUSAS PRINCIPAIS, PORTANTO UMA QUANTIDADE PEQUENA DE FATORES INFLUÊNCIA NO RESULTADO MAIOR 
+
+"80% PIB É PROVENIENTES DE 20% DO PAÍS"
+
+Então basicamente o Princípio de Pareto é poderoso pra análise e tomada de
+decisões, porque aqui a gente consegue focar nos 20%, ou seja, focar nos fatores
+mais importantes, e com isso conseguir a maior parte dos resultados (80%), com
+menos tempo e esforço.
+
+FOQUE NOS PRINCIPAIS INDICADORES PARA TOMADA DE DECISÃO
+
+*   80% dos insights úteis podem vir de 20% dos dados analisados.
+*   80% dos problemas identificados podem ser atribuídos a 20% das causas.
+
+-- CASE DE AUMENTAR A PRODUTIVIDADE DA FÁBRICA: 
+1ª RECEITA MENSAL
+2ª CUSTO MENSAL
+3ª FATURAMENTO MENSAL
+4ª RECEITA POR PRODUTO
+5ª ABERTURA DE CUSTO
+ETC
+
+
+-- 3ª E 4ª COMO ESSES 80% IMPACTAM NOS INDICADORES E VISUALIZAÇÕES
+{VER VÍDEO}
+
+RESUMO: LUCRO DA EMPRESA EM QUEDA, FATURAMENTO MENSAL ESTÁVEL, CUSTO MENSAL EM ALTA, SEGMENTANDO A ANÁLISE É VISTO QUE O CUSTO DA MATÉRIA PRIMA AUMENTOU.
+NO CASO O USO DE AÇUCAR E OUTROS INGREDIENTES. DEVIDO ISSO A FÁBRICA PLANEJA DE ALGUM MODO MANTER A LUCRATIVIDADE SEM AFETAR O PRODUTO. O QUE GERA HIPÓTESES DE COMO
+SOLUCIONAR E ALCANÇAR O OBJETIVO.
+
+Entrando na raiz do problema, dá pra perceber que é a matéria prima
+da bebida.
+
+Mais especificamente, açúcar e plástico
+Conseguimos trocar de fornecedor? Reduzir a quantidade dos insumos
+sem afetar a qualidade?
+
+Resultado: não conseguimos trocar de fornecedor de açúcar, que é um
+no Brasil, nem reduzir a quantidade sem perder qualidade.
+
+Deu para cortar pela metade a quantidade de plástico na embalagem, reduzindo
+em 50% o custo de uma das principais linhas de custo da fábrica (e ainda usa como
+branding)
+
+a) Levantar as suas hipóteses
+b) Trazer dados para comprovar essas hipóteses
+c) Correlação x Causalidade
