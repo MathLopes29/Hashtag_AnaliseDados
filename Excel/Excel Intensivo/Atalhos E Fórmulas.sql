c91d1ff8c0d4ff198c729ec05c68a7d81c3eb78a
Matheus Lopes <mat.lopes2907@gmail.com>
2025-04-26 19:46:30 -0300

    Estudos

--- Excel/Excel Intensivo/Atalhos E Fórmulas.sql
+++ Excel/Excel Intensivo/Atalhos E Fórmulas.sql
@@ -31,8 +31,8 @@ SE
 SEEROR
 PROCV
 SUBTOTAL
-ÍNDICE
-CORRESP
+ÍNDICE ***** Busca / Procura
+CORRESP ***** Posição
 MAX
 MIN
 MAIOR
@@ -43,6 +43,17 @@ CONT.VAZIO
 CONT.SE
 E
 
+/*******************************/
+PROCX VERSUS PROCV + CORRESP
+
+=PROCV($B16;$B$2:$H$13;CORRESP(D$15;$B$2:$H$2;0);0)
+=PROCX($B16;$B$3:$B$13;$C$3:$H$13;0;0;1)
+/***********************************/
+
+
+
+=ÍNDICE($D:$D;CORRESP($G$1;$B:$B;0))
+=PROCX(1;($B$7:$B$405=B4)*($C$7:$C$405=A4)*($A$7:$A$405=C4);$D$7:$D$405;0;0;1)
 =SEERRO(SE(PROCV($B$7;Base!D:D;1;0)=$B$7;"Encontrado");"Não encontrado")
 =CONT.SES(A:A;A1)<2
 =CONT.SES(E:E;">31/12/2018")
