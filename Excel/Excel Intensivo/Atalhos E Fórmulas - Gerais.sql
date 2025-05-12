c6f8670c5d3dc55fdebe9855aa88768ead7fcce0
Matheus Lopes <mat.lopes2907@gmail.com>
2025-05-12 15:49:38 -0300

    Gráficos 5

---
+++ Excel/Excel Intensivo/Atalhos E Fórmulas - Gerais.sql
@@ -0,0 +1,54 @@
+Ctrl + A -- Abre uma pasta do Excel
+Ctrl + B -- Salva a pasta atual
+Ctrl + C -- Copiar
+Ctrl + D -- Preenche a célula atual a partir da célula acima
+Ctrl + E -- Otimiza tarefas de manipulação e formatação de dados
+Ctrl + I -- Itálico
+Ctrl + K -- Abre a janela de criação e edição de hiperlink
+Ctrl + L -- Abre a janela de localização
+Ctrl + N -- Negrito
+Ctrl + O -- Cria uma pasta do Excel
+Ctrl + P -- Imprimir 
+Ctrl + R -- Preenche a célula atual a partir da célula acima à esquerda
+Ctrl + S -- Sublinhado
+Ctrl + T -- Selecione todas as células na planilha
+Ctrl + U -- Abre a janela de substituição
+Ctrl + V -- Colar
+Ctrl + W -- Fecha a pasta atual
+Ctrl + X -- Recortar
+Ctrl + Y -- Refaz a última edição 
+Ctrl + Z -- Desfaz a última edição
+
+Ctrl + Alt + T -- Insere uma nova tabela na planilha
+Ctrl + Espaço -- Seleciona toda a coluna atual
+Ctrl + F1 -- Alterna entre expandir e recolher a faixa de opções
+Ctrl + F3 -- Gerenciador de Nomes do Excel 
+Ctrl + Shif + L -- Alterna desativando e ativando o AutoFiltro do Excel
+Ctrl + Shift + A -- Insere o mapa de argumentos de uma função
+Ctrl + Shift + F -- Abre a janela de formatação de célula
+Ctrl + Shift + F1 -- Oculta a faixa de opções
+Ctrl + Shift + F3 -- Abre a janela para criar nomes a partir da seleção atual
+Ctrl + Shift + F4 -- Localizar combinação anterior
+Ctrl + Shift + U -- Expande ou recolhe a caixa de edição barra de fórmulas
+
+F1 -- Ajuda
+F2 -- Inicia a edição da célula selecionada
+F3 -- Abre o selecionador de nomes para inserir nome na fórmula
+F4 -- Alternar entre referências absolutas e relativas
+F5 -- Abre o menu de Ir para
+F6 -- Move para próximo painel da janela alternando entre guia, faixa de opções, planilha e barra de status
+F7 -- Abre o painel de verificação de ortografia
+F8 -- Aciona o modo de seleção estendida
+F9 -- Calcula todas as planilhas abertas
+F10 -- Abre o menu superior de comandos
+F11 -- Cria um gráfico ou tabela dinâmica em nova planilha 
+F12 -- Salva a pasta atual como
+
+Shift + Espaço -- Seleciona toda a linha atual
+Shift + F2 -- Insere ou edita anotação de comentário na célula
+Shift + F3 -- Abre a janela para inserir funções do Excel
+Shift + F4 -- Localiza a próxima combinação
+Shift + F8 -- Alterna entre adicionar ou remover o modo de seleção
+Shift + F9 -- Calcula apenas a planilha selecionada
+Shift + Home -- Seleciona até o início da célula
+Shift + Tab -- Move o cursor de seleção para a esquerda.
