c9036a92560ca42b619e513be5f6802f6426d460
Matheus Lopes <mat.lopes2907@gmail.com>
2025-06-04 21:31:13 -0300

    Ajustes

---
+++ Power BI/Trilha/Storytelling/Storytelling.sql
@@ -0,0 +1,26 @@
+/* TIPOS DE GRÁFICOS */
+Linhas * Área - Evolução de valores ao longo do tempo			
+Colunas * Barras - Comparação e Ranking	
+Mapa de Árvore * Explosão Solar - Dados hierárquicos		
+Dispersão - Valores Cartesianos
+Cascata - Evolução de valores	
+Pizza * Rosca - Partes de um total	
+Funil - Afunilamento de valores	
+Superfície
+Radar * Pareto - Comparação	
+Bolhas	
+Mapa
+Histograma	
+Ação
+
+
+/* POWER BI */
+* Visualização de Leitura em Z (Esquerda para a Direita, Cima para Baixo)
+* Classificação dos Gráficos
+* Contextualizar as informações
+
+* Gráficos Temporais: Gráfico de Linha, Coluna e Área -> Noção de Continuidade / Evolução
+* Gráficos Temporais Comparativo: Gráfico de Colunas Clusterizadas 
+* Gráficos de Escalas diferentes: Gráfico Combinado (Linha + Coluna)
+* Gráfico de Etapas / Funil:  Afunilamento de valores	
+-- O restante segue a mesma lógica do Excel!
